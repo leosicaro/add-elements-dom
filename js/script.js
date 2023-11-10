6d7267cf0073548d4f5210a51e7907591f496920
Leonel Sicaro <leonelsicaro@gmail.com>
2023-11-11 00:19:05 +0100

    reto2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
  // Aquí tu código
+let button = document.getElementById("agregar");
+let lista = document.getElementById("lista");
+
+button.addEventListener("click",function(){
+  console.log("click")
+  let item = prompt("list")
+  let li = document.createElement("li")
+  let h2 = document.createElement("h2")
+  h2.textContent = item
+  li.appendChild(h2)
+  lista.appendChild(li)
+  
+  console.log(item)
+    
+})
+// aun me noto me noto mas verde que este comentario en CSS luego lo pongo bonito cuando este mas preparado IpromiseYou
+
